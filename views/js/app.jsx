a08a336e78104bd72c4d7c854f2884bd0ac1787d
Madhur batra <madhurbatra097@gmail.com>
2021-01-12 16:35:09 +0530

    Minor updates

--- views/js/app.jsx
+++ views/js/app.jsx
@@ -23,6 +23,35 @@ class Home extends React.Component {
     }
 }
 
+class Joke extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { liked: "" }
+        this.like = this.like.bind(this)
+    }
+    like() {
+
+    }
+    render() {
+        return (
+            <div className="col-xs-4">
+                <div className="panel panel-default">
+                    <div className="panel-heading">#{this.props.joke.id} <span className="pull-right">{this.state.liked}</span></div>
+                    <div className="panel-body">
+                        {this.props.joke.joke}
+                    </div>
+                    <div className="panel-footer">
+                        {this.props.joke.likes} Likes &nbsp
+                        <a onClick={this.like} className="btn btn-default">
+                            <span className="glyphicon glyphicon-thumbs-up"></span>"
+                        </a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
 class LoggedIn extends React.Component {
     constructor(props) {
         super(props);
@@ -30,3 +59,24 @@ class LoggedIn extends React.Component {
             jokes: []
         }
     }
+    render() {
+        return (
+            <div className="container">
+                <div className="col-lg-12">
+                    <br />
+                    <span className="pull-right"> <a onClick={this.logout}>Log Out</a></span>
+                    <h2>Jokeish</h2>
+                    <p>Let's feed you with some funny Jokes!!!</p>
+                    <div className="row">
+                        {this.state.jokes.map(function (joke, i) {
+                            return (<Joke key={i} joke={joke} />);
+                        })}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("App"))
+//export default App
